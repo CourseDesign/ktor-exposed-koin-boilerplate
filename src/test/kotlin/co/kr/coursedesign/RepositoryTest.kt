ff4df30dd89255e8350821813580356b934470e0
siyual-park <siyual.bak@gmail.com>
2021-01-18 17:04:21 +0900

    Add Patcher

--- src/test/kotlin/co/kr/coursedesign/RepositoryTest.kt
+++ src/test/kotlin/co/kr/coursedesign/RepositoryTest.kt
@@ -2,12 +2,15 @@ package co.kr.coursedesign
 
 import co.kr.coursedesign.database.repository.findById
 import co.kr.coursedesign.database.repository.save
+import co.kr.coursedesign.database.repository.updateById
 import co.kr.coursedesign.model.City
+import co.kr.coursedesign.model.CityPatch
 import co.kr.coursedesign.model.User
 import co.kr.coursedesign.repository.CityRepository
 import co.kr.coursedesign.repository.UserRepository
 import co.kr.coursedesign.table.Cities
 import co.kr.coursedesign.table.Users
+import java.util.Optional
 import kotlin.test.Test
 import kotlin.test.assertEquals
 import kotlin.test.assertNotNull
@@ -60,6 +63,34 @@ class RepositoryTest {
 
             assert(!cityRepository.existsById(city.id!! + 1))
             assert(!userRepository.existsById(user.id!! + 1))
+        }
+    }
+
+    @Test
+    fun testUpdate() {
+        withTables(Users, Cities) {
+            val cityRepository = CityRepository(this.db)
+
+            val city = cityRepository.save(City("Test"))
+            val newCity: City = cityRepository.updateById(
+                city.id!!,
+                CityPatch(
+                    Optional.of("New")
+                )
+            )
+            assertEquals(newCity.id, city.id)
+            assertEquals(newCity.name, "New")
+        }
+    }
+
+    @Test
+    fun testDelete() {
+        withTables(Users, Cities) {
+            val cityRepository = CityRepository(this.db)
+
+            val city = cityRepository.save(City("Test"))
+            cityRepository.deleteById(city.id!!)
+            assert(!cityRepository.existsById(city.id!!))
         }
     }
 }
---
+++ src/test/kotlin/co/kr/coursedesign/model/CityPatch.kt
@@ -0,0 +1,7 @@
+package co.kr.coursedesign.model
+
+import java.util.Optional
+
+data class CityPatch(
+    var name: Optional<String> = Optional.empty(),
+)
